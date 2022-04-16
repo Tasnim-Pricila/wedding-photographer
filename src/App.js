2fed8973e74f4523d51d4886f0e8054820fd65c7
unknown <tasnimtanzim55@gmail.com>
2022-04-17 02:30:34 +0600

    Navbar and Routing Added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './components/About/About';
+import Blog from './components/Blog/Blog';
+import Home from './components/Home/Home';
+import Login from './components/Login/Login';
+import NotFound from './components/NotFound/NotFound';
+import Signup from './components/Signup/Signup';
+import Header from './shared/Header/Header';
 
 function App() {
   return (
-    <div>
-      
+    <div className='container mx-auto'>
+        <Header></Header>
+        <Routes>
+            <Route path='/' element={<Home></Home>}></Route>
+            <Route path='/home' element={<Home></Home>}></Route>
+            <Route path='/blogs' element={<Blog></Blog>}></Route>
+            <Route path='/about' element={<About></About>}></Route>
+            <Route path='/login' element={<Login></Login>}></Route>
+            <Route path='/signup' element={<Signup></Signup>}></Route>
+            <Route path='*' element={<NotFound></NotFound>}></Route>
+        </Routes>
     </div>
   );
 }
---
+++ src/shared/Header/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <>
+            <div className='flex justify-between py-2.5 px-12 '>
+                <div>
+                    <h2 className='text-2xl uppercase font-bold'>Memory Makers</h2>
+                    <small className='text-sm tracking-widest uppercase'>Wedding Photographer</small>
+                </div>
+                <div className='flex gap-5 font-medium items-center uppercase'>
+                    <NavLink to='/' className={({isActive}) => (isActive ? "text-fuchsia-700 font-bold" : "")}>Home</NavLink>
+                    <NavLink to='/blogs' className={({isActive}) => (isActive ? "text-fuchsia-700 font-bold" : "")}>Blog</NavLink>
+                    <NavLink to='/about' className={({isActive}) => (isActive ? "text-fuchsia-700 font-bold" : "")}>About Me</NavLink>
+                    <NavLink to='/login' className={({isActive}) => (isActive ? "text-fuchsia-700 font-bold" : "")}>Login</NavLink>
+                    <NavLink to='/signup' className={({isActive}) => (isActive ? "text-fuchsia-700 font-bold" : "")}>Signup</NavLink>
+                </div>
+            </div>
+            
+        </>
+    );
+};
+
+export default Header;
