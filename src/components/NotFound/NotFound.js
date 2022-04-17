1dfa26d8f33f9fdd8f42cc8e4a5dd1ced3487f15
unknown <tasnimtanzim55@gmail.com>
2022-04-17 20:14:05 +0600

    404 page styled

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
     return (
-        <div>
-            Error
-        </div>
+        <>
+            <div className='uppercase mx-auto w-1/2 py-12'>
+                <p className='tracking-[20px] text-center text-9xl my-12'>OOPS!</p>
+                <p className='tracking-wider text-center text-[red] font-bold text-9xl'>404</p>
+                <p className='tracking-wider text-center text-5xl my-6'>Page not found</p>
+                <p className='text-center my-12'>It seems you've venured too far...</p>
+                
+            </div>
+        </>
     );
 };
 
--- src/shared/Footer/Footer.js
+++ src/shared/Footer/Footer.js
@@ -1,4 +1,4 @@
-import { faFacebook, faFacebookF, faGoogle, faGooglePlus, faGooglePlusG, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faFacebookF, faGooglePlusG, faTwitter } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope, faLocationPin, faPhone } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
@@ -9,18 +9,18 @@ const Footer = () => {
     const year = today.getFullYear();
     return (
         <>
-            <div className='grid grid-cols-4 bg-[#2C2727] px-24 gap-12 py-20 justify-center'>
+            <div className='grid grid-cols-4 bg-[#2C2727] px-28 gap-12 py-20 justify-center'>
                 <div>
                     <div className='text-white'>
-                        <h2 className='text-2xl uppercase font-bold '>Memory Makers</h2>
-                        <small className='text-sm tracking-widest uppercase '>Wedding Photographer</small>
+                        <h2 className='text-2xl uppercase font-bold tracking-widest'>TASNIM PRICILA</h2>
+                        <small className='text-sm tracking-widest uppercase pl-1'>Wedding Photographer</small>
                     </div>
                     <p className='text-[#ABA9A9] mt-8 justify-items-center'>
                         When you found me, you know very well that you have found your princess. Because your smile is not only on your face but also in your heart and in your very being…
                     </p>
                 </div>
 
-                <div className=' justify-self-center'>
+                <div className='justify-self-center'>
                     <p className='text-xl text-white'>Company</p>
                     <ul className='mt-12 text-[#ABA9A9] leading-9'>
                         <li>
@@ -35,7 +35,7 @@ const Footer = () => {
                     </ul>
                 </div>
 
-                <div className=''>
+                <div className='justify-self-center'>
                     <p className='text-xl text-white'>Support</p>
                     <ul className='mt-11 text-[#ABA9A9] leading-9'>
                         <li>
@@ -52,7 +52,7 @@ const Footer = () => {
                         </li>
                     </ul>
                 </div>
-                <div className=''>
+                <div className='justify-self-end'>
                     <p className='text-xl text-white'>Contact</p>
                     <div className='mt-11 text-[#ABA9A9] leading-9'>
                         <div className='flex gap-3 items-center'>
